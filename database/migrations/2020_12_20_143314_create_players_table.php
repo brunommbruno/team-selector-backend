cf75cddde8c0e2c604d8b49d3ae1a416964872a7
brunommbruno <brunommbruno@gmail.com>
2020-12-20 14:34:59 +0000

    created player model migration

---
+++ database/migrations/2020_12_20_143314_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("player_name", 20);
+            $table->integer("skill");
+            $table->string("position", 3);
+            $table->foreignId("team_id")
+            ->constrained()
+            ->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
